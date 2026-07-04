56d559b4790340183130f41548868eba101b3c1d
MdAffan009 <mdaffan0502@gmail.com>
2026-07-04 12:47:36 +0530

    Added Deletion Logic

--- Arrays/operations.c
+++ Arrays/operations.c
@@ -2,16 +2,16 @@
 
 int main()
 {
-    int arr[4] = {2, 4, 8};
+    int arr[6] = {2, 4, 8, 12, 16};
 
     //Updating the element, takes O(1)
     arr[1] = 6;
 
 
-    //Insertion of the element if there is a capcity, in worst case it takes O(n)
+    //Insertion of the element if there is a capcity, takes O(n)
     //Let's say we have to add 4 at index 1
 
-    int size = 3;
+    int size = 5;
     int index = 1;
     int value = 4;
 
@@ -28,6 +28,25 @@ int main()
         printf("%d \n", arr[i]);
     }
     
+    printf("\n");
+    printf("---------------------------------------------------------------------------------------------- \n");
+
+    //Deletion of the element too takes O(n)
+    //Let's say we have to delete first element
+
+    index = 2; //We will reuse the value of size from above
+
+    for (int i = index; i < size - 1; i++)
+    {
+        arr[i] = arr[i + 1]; //This will shift elements to left which results in index overwriting
+    }
+
+    size--;
+    
+    for (int  i = 0; i < size; i++)
+    {
+        printf("%d \n", arr[i]);
+    }
 
     return 0;
 }
