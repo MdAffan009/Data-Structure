89958024c0eda58b4f7166f3147c1a830a21f944
MdAffan009 <mdaffan0502@gmail.com>
2026-07-22 18:56:02 +0530

    Implemented Pop

--- Stack/main.c
+++ Stack/main.c
@@ -10,11 +10,13 @@ typedef struct Stack
     int top;
 } Stack;
 
+// Initialzation
 void initStack(Stack *stack)
 {
     stack->top = -1;
 }
 
+// Length Check
 bool isEmpty(Stack *stack)
 {
     return stack->top == -1;
@@ -25,9 +27,10 @@ bool isFull(Stack *stack)
     return stack->top == MAX_SIZE - 1;
 }
 
+// Push(Adding on the top)
 void push(Stack *stack, int value)
 {
-    if (stack->top == MAX_SIZE - 1)
+    if (isFull(stack))
     {
         printf("Stack Overflow!! \n");
         return;
@@ -36,7 +39,19 @@ void push(Stack *stack, int value)
     stack->data[++stack->top] = value;
 }
 
+// Pop(Deleting the top)
+void pop(Stack *stack)
+{
+    if (isEmpty(stack))
+    {
+        printf("Stack Underflow\n");
+        return;
+    }
 
+    stack->top--;
+}
+
+// Displaying the top element
 void peek(Stack *stack)
 {
     if (isEmpty(stack))
@@ -48,6 +63,21 @@ void peek(Stack *stack)
     printf("%d \n", stack->data[stack->top]);
 }
 
+// Traversing through stack
+void display(Stack *stack)
+{
+    if (isEmpty(stack))
+    {
+        printf("Stack is empty\n");
+        return;
+    }
+
+    for (int i = stack->top; i >= 0; i--)
+    {
+        printf("%d\n", stack->data[i]);
+    }
+}
+
 int main()
 {
     Stack stack;
@@ -55,8 +85,17 @@ int main()
     isEmpty(&stack);
     initStack(&stack);
 
+    push(&stack, 10);
+    push(&stack, 20);
     push(&stack, 30);
+    push(&stack, 40);
+
     peek(&stack);
+    pop(&stack);
+  
+    printf("\n");
+
+    display(&stack);
 
     return 0;
 }
